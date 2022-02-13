e35ca6f159c92d1a8dc0e9fcdaff7cde04f604c1
Dinesh Pandiyan <flexdinesh@gmail.com>
2022-02-13 17:00:55 +1100

    Update title

--- apps/boilerplate-nextjs/src/components/Meta.tsx
+++ apps/boilerplate-nextjs/src/components/Meta.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head';
 const Meta = () => {
   return (
     <Head>
-      <title>Design System Boilerplate - Next.js</title>
+      <title>Design System Boilerplate - Next.js Example</title>
       <meta
         name="description"
         content="Example next.js boilerplate that consumes our design system"
